f35cc6c86763ac5e6be9f226536cf97dfb8c8307
kewes <kewes58@gmail.com>
2018-10-29 08:01:40 +0700

    upload pertama

---
+++ src/snapapp09/SnapApp09.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snapapp09;
+
+/**
+ *
+ * @author Kewes
+ */
+public class SnapApp09 extends SnapFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SnapApp09 snap = new SnapApp09 ();
+        snap.setVisible(true);
+    }
+    
+}
